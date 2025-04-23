068be9fed28f1b623deb79e52b308bcbacd537ed
NyFilip <filip.nyman@hotmail.se>
2025-04-23 09:59:29 +0200

    Added personal files

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-print("Hello World!")
+catsAndDogs = np.genfromtxt('catdogdata.txt')
+numbers=np.genfromtxt('Numbers.txt')
+print(catsAndDogs.shape,numbers.shape)
+print(catsAndDogs,numbers)
