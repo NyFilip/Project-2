11af9a686e9a07cbffa6f64a3df836b021e3bf27
nlennartsson <natanael.c.lennartsson@gmail.com>
2025-04-28 15:48:41 +0200

    fixed and error in dataset.py and also shuffeled the catdog data set

--- dataSet.py
+++ dataSet.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 
-def mnist(filePath):
+def mnist(filePath = 'Numbers.txt'):
     imagesList = []
     imagesMatrix = []
     labels = []
@@ -31,12 +31,13 @@ def mnist(filePath):
     imagesList = np.array(imagesList)                    
     return full, labels, imagesMatrix, imagesList
             
-def catdog(filePath):
+def catdog(filePath = 'catdogdata.txt'):
     labels = []
     labels[:98] = np.zeros(99)
     labels[99:] = np.ones(99)
-    imagesMatrix = []
-    imagesList = []
+    sLabels = []
+    sImagesMatrix = []
+    sImagesList = []
     full = []
     i = 0
     with open(filePath, 'r') as file:
@@ -46,9 +47,6 @@ def catdog(filePath):
             index = parts[0].strip()
             pixels = list(map(int,parts[1:]))
 
-            imagesMatrix.append(np.array(pixels).reshape(64,64))
-            imagesList.append(np.array(pixels))
-
             fullTemp = list(pixels)
             if i < 99:
                 fullTemp.insert(0, 0)
@@ -56,13 +54,21 @@ def catdog(filePath):
                 fullTemp.insert(0,1)
             full.append(fullTemp)
             i += 1
-
-    imagesMatrix = np.array(imagesMatrix)
-    imagesList = np.array(imagesList)
     full = np.array(full)
-    labels = np.array(labels)
-    return full, labels, imagesMatrix, imagesList
 
+    np.random.shuffle(full)
 
-if __name__ == '__main__':
+    for line in full:
+        slabel = line[0]
+        spixels = line[1:]
+        sLabels.append(slabel)
+        sImagesList.append(spixels)
+        sImagesMatrix.append(np.array(spixels).reshape(64,64))
+    sLabels = np.array(sLabels)
+    sImagesList = np.array(sImagesList)
+    sImagesMatrix = np.array(sImagesMatrix)
+    
+    
+    return full, sLabels, sImagesMatrix, sImagesList
+
 
